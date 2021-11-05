e94af4b95f042b873d3b91e88c11f1f0a551358b
eunbin1119 <92962043+eunbin1119@users.noreply.github.com>
2021-11-05 10:07:30 +0900

    장면 1,2 디자인 끝
    메인 수정/ Scene패키지 만듦

---
+++ CookingKingatHome/src/application/Main.java
@@ -0,0 +1,31 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("/Scene/MainScene.fxml"));	
+			AnchorPane ap = (AnchorPane)loader.load();
+			Scene scene = new Scene(ap,600,400);			
+			
+			primaryStage.setTitle("방구석 요리왕");
+			
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
