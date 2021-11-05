0f8fc05cd16dba79f90bef7a5a04e657b1863d65
神武天皇 <92962086+DongMyungSungKing@users.noreply.github.com>
2021-11-05 11:19:29 +0900

    Add files via upload

---
+++ recipes.sql
@@ -0,0 +1,38 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- 생성 시간: 21-11-05 03:19
+-- 서버 버전: 10.4.20-MariaDB
+-- PHP 버전: 7.4.22
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 데이터베이스: `recipes`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 테이블 구조 `cookingrecipe`
+--
+
+CREATE TABLE `cookingrecipe` (
+  `RecipeName` text NOT NULL,
+  `Recipe` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
