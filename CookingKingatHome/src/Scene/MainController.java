bedad6a226b5d0e2dc6938d073c7b9c54878152c
eunbin1119 <92962043+eunbin1119@users.noreply.github.com>
2021-11-05 11:10:15 +0900

    디자인 수정
    디자인 수정

--- CookingKingatHome/src/Scene/MainController.java
+++ CookingKingatHome/src/Scene/MainController.java
@@ -1,5 +1,30 @@
 package Scene;
 
-public class MainController {
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+public class MainController {
+	// 화면 전환 메소드
+		public void changScene(String url, Button btn) {
+			try {
+				Parent main = FXMLLoader.load(getClass().getResource(url));
+				Scene scene = new Scene(main);
+				Stage primaryStage = (Stage) btn.getScene().getWindow();
+				primaryStage.setScene(scene);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		// 시작 => 메인2
+		@FXML
+		private Button Main2But;
+
+		public void MAin2Scene() {
+			changScene("/Scene/MainScene2.fxml", Main2But);
+		}
 }
